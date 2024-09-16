898c4cf0947efac653519d39c471e09f1d576ee8
Mc Isti <90382294+mcisti@users.noreply.github.com>
2024-09-16 14:17:36 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,48 +1,15 @@
-﻿using System;
-using IronXL;
+﻿using IronXL;
+using System;
+using System.Linq;
 
-namespace ExcelExportApp
+License.LicenseKey = "IRONSUITE.ZADORT.KKSZKI.HU.26764-DEC9CFA078-BDGVTOX-4S5W2SYZ734V-62QGSBQ5THZG-6BDGF57AKERJ-TSTE7QBH7XX7-RWNJ33MXTHMJ-H32SW3PB4RO6-22Q4SD-TWTCQIBBCJ2NUA-DEPLOYMENT.TRIAL-NM5PSK.TRIAL.EXPIRES.13.OCT.2024";
+WorkBook workBook = WorkBook.Load("mintainput.xlsx");
+WorkSheet workSheet = workBook.WorkSheets[0];
+WorkSheet firstSheet = workBook.DefaultWorkSheet;
+
+int cellValue = workSheet["A2"].IntValue;
+
+foreach (var cell in workSheet["A1:B2"])
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            License.LicenseKey = "IRONSUITE.ZADORT.KKSZKI.HU.26764-DEC9CFA078-BDGVTOX-4S5W2SYZ734V-62QGSBQ5THZG-6BDGF57AKERJ-TSTE7QBH7XX7-RWNJ33MXTHMJ-H32SW3PB4RO6-22Q4SD-TWTCQIBBCJ2NUA-DEPLOYMENT.TRIAL-NM5PSK.TRIAL.EXPIRES.13.OCT.2024";
-
-            var workBook = WorkBook.Create();
-            var workSheet = workBook.DefaultWorkSheet;
-
-            Console.WriteLine("Adja meg a sorok számát:");
-            if (!int.TryParse(Console.ReadLine(), out int numberOfRows) || numberOfRows <= 0)
-            {
-                Console.WriteLine("Érvénytelen sor szám.");
-                return;
-            }
-
-            workSheet["A1"].Value = "Név";
-            workSheet["B1"].Value = "Kor";
-
-            for (int i = 0; i < numberOfRows; i++)
-            {
-                Console.WriteLine($"Adja meg az {i + 1}. sor nevét:");
-                var name = Console.ReadLine();
-
-                Console.WriteLine($"Adja meg az {i + 1}. sor korát:");
-                if (!int.TryParse(Console.ReadLine(), out int age) || age < 0)
-                {
-                    Console.WriteLine("Érvénytelen kor.");
-                    return;
-                }
-
-                workSheet[$"A{i + 2}"].Value = name;
-                workSheet[$"B{i + 2}"].Value = age;
-            }
-
-
-            var filePath = "exportált_adatok.xlsx";
-            workBook.SaveAs(filePath);
-
-            Console.WriteLine($"Az adatok sikeresen mentve: {filePath}");
-        }
-    }
+    Console.WriteLine("Cell {0} has value '{1}'", cell.AddressString, cell.Text);
 }
---
+++ Program1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using IronXL;
+
+namespace ExcelExportApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            License.LicenseKey = "IRONSUITE.ZADORT.KKSZKI.HU.26764-DEC9CFA078-BDGVTOX-4S5W2SYZ734V-62QGSBQ5THZG-6BDGF57AKERJ-TSTE7QBH7XX7-RWNJ33MXTHMJ-H32SW3PB4RO6-22Q4SD-TWTCQIBBCJ2NUA-DEPLOYMENT.TRIAL-NM5PSK.TRIAL.EXPIRES.13.OCT.2024";
+
+            var workBook = WorkBook.Create();
+            var workSheet = workBook.DefaultWorkSheet;
+
+            Console.WriteLine("Adja meg a sorok számát:");
+            if (!int.TryParse(Console.ReadLine(), out int numberOfRows) || numberOfRows <= 0)
+            {
+                Console.WriteLine("Érvénytelen sor szám.");
+                return;
+            }
+
+            workSheet["A1"].Value = "Név";
+            workSheet["B1"].Value = "Kor";
+
+            for (int i = 0; i < numberOfRows; i++)
+            {
+                Console.WriteLine($"Adja meg az {i + 1}. sor nevét:");
+                var name = Console.ReadLine();
+
+                Console.WriteLine($"Adja meg az {i + 1}. sor korát:");
+                if (!int.TryParse(Console.ReadLine(), out int age) || age < 0)
+                {
+                    Console.WriteLine("Érvénytelen kor.");
+                    return;
+                }
+
+                workSheet[$"A{i + 2}"].Value = name;
+                workSheet[$"B{i + 2}"].Value = age;
+            }
+
+
+            var filePath = "exportált_adatok.xlsx";
+            workBook.SaveAs(filePath);
+
+            Console.WriteLine($"Az adatok sikeresen mentve: {filePath}");
+        }
+    }
+}
